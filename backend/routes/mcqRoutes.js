bf7a9b9a929f59a3e97e9fad99aea2af44dad647
SujalNimbalkar <163315046+SujalNimbalkar@users.noreply.github.com>
2025-07-07 14:26:06 +0530

    Add files via upload

--- backend/routes/mcqRoutes.js
+++ backend/routes/mcqRoutes.js
@@ -102,7 +102,7 @@ router.get("/employee_assessment_results/all", async (req, res) => {
     const sheets = await getSheetsClient();
     const response = await sheets.spreadsheets.values.get({
       spreadsheetId: SCORE_LOG_SHEET_ID,
-      range: "Sheet1!A2:J", // Adjust range as needed
+      range: "Sheet1!A2:K", // Adjust range as needed
     });
     const rows = response.data.values || [];
     const results = rows.map((row) => ({
@@ -116,6 +116,7 @@ router.get("/employee_assessment_results/all", async (req, res) => {
       max_score: row[7],
       percent: row[8],
       status: row[9],
+      StrLevel: row[10],
     }));
     res.json(results);
   } catch (e) {
